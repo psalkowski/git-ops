92ea344315c636e97efab661158ea207b958a2da
Piotr Sałkowski <skc.peter@gmail.com>
2024-04-16 10:18:31 +0200

    Increase bitwarden memory limit to 1Gi for testing purpose

--- docker/bitwarden-cli/entrypoint.sh
+++ docker/bitwarden-cli/entrypoint.sh
@@ -8,5 +8,6 @@ export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
 
 bw unlock --check
 
-echo 'Running `bw server` on port 8087'
-bw serve --hostname 0.0.0.0 #--disable-origin-protection
+echo: ""
+echo "Running 'bw server' on port 8087"
+bw serve --hostname 0.0.0.0
